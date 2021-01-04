5ce5b3f3b954e191edfd5b50de114f746611c18b
Ubuntu <ubuntu@ip-172-31-20-109.ec2.internal>
2021-01-04 03:17:09 +0000

    edicion de llaves

--- app/Http/Controllers/AdafruitController.php
+++ app/Http/Controllers/AdafruitController.php
@@ -9,35 +9,35 @@ class AdafruitController extends Controller
 {
     public function get_bano(){
         $response = Http::withHeaders([
-            'X-AIO-Key' => 'aio_PBsQ69yddxty8iPEmJ3JCY1D9GRP',
+            'X-AIO-Key' => 'aio_pwkw35gZUnGa7x9PJI2emdhisicX',
         ])->get('https://io.adafruit.com/api/v2/JuanVazquez/feeds/proyectofinal.led-bano');
         return $response;
     }
 
 public function get_cocina(){
         $response = Http::withHeaders([
-            'X-AIO-Key' => 'aio_PBsQ69yddxty8iPEmJ3JCY1D9GRP',
+            'X-AIO-Key' => 'aio_pwkw35gZUnGa7x9PJI2emdhisicX',
         ])->get('https://io.adafruit.com/api/v2/JuanVazquez/feeds/proyectofinal.led-cocina');
         return $response;
     }
 
 public function get_oficina(){
         $response = Http::withHeaders([
-            'X-AIO-Key' => 'aio_PBsQ69yddxty8iPEmJ3JCY1D9GRP',
+            'X-AIO-Key' => 'aio_pwkw35gZUnGa7x9PJI2emdhisicX',
         ])->get('https://io.adafruit.com/api/v2/JuanVazquez/feeds/proyectofinal.led-oficina');
         return $response;
     }
 
 public function get_cuarto(){
         $response = Http::withHeaders([
-            'X-AIO-Key' => 'aio_PBsQ69yddxty8iPEmJ3JCY1D9GRP',
+            'X-AIO-Key' => 'aio_pwkw35gZUnGa7x9PJI2emdhisicX',
         ])->get('https://io.adafruit.com/api/v2/JuanVazquez/feeds/proyectofinal.led-cuarto');
         return $response;
     }
 
 public function get_sala(){
         $response = Http::withHeaders([
-            'X-AIO-Key' => 'aio_PBsQ69yddxty8iPEmJ3JCY1D9GRP',
+            'X-AIO-Key' => 'aio_pwkw35gZUnGa7x9PJI2emdhisicX',
         ])->get('https://io.adafruit.com/api/v2/JuanVazquez/feeds/proyectofinal.led-sala');
         return $response;
     }
@@ -45,7 +45,7 @@ public function get_sala(){
 
     public function led_bano(Request $request){
         $response = Http::withHeaders([
-            'X-AIO-Key' => 'aio_PBsQ69yddxty8iPEmJ3JCY1D9GRP',
+            'X-AIO-Key' => 'aio_pwkw35gZUnGa7x9PJI2emdhisicX',
         ])->post('https://io.adafruit.com/api/v2/JuanVazquez/feeds/proyectofinal.led-bano/data',[
             'value'=>$request->value
         ]);
@@ -54,7 +54,7 @@ public function get_sala(){
 
     public function led_cocina(Request $request){
         $response = Http::withHeaders([
-            'X-AIO-Key' => 'aio_PBsQ69yddxty8iPEmJ3JCY1D9GRP',
+            'X-AIO-Key' => 'aio_pwkw35gZUnGa7x9PJI2emdhisicX',
         ])->post('https://io.adafruit.com/api/v2/JuanVazquez/feeds/proyectofinal.led-cocina/data',[
             'value'=>$request->value
         ]);
@@ -63,7 +63,7 @@ public function get_sala(){
 
     public function led_cuarto(Request $request){
         $response = Http::withHeaders([
-            'X-AIO-Key' => 'aio_PBsQ69yddxty8iPEmJ3JCY1D9GRP',
+            'X-AIO-Key' => 'aio_pwkw35gZUnGa7x9PJI2emdhisicX',
         ])->post('https://io.adafruit.com/api/v2/JuanVazquez/feeds/proyectofinal.led-cuarto/data',[
             'value'=>$request->value
         ]);
@@ -72,7 +72,7 @@ public function get_sala(){
 
     public function led_oficina(Request $request){
         $response = Http::withHeaders([
-            'X-AIO-Key' => 'aio_PBsQ69yddxty8iPEmJ3JCY1D9GRP',
+            'X-AIO-Key' => 'aio_pwkw35gZUnGa7x9PJI2emdhisicX',
         ])->post('https://io.adafruit.com/api/v2/JuanVazquez/feeds/proyectofinal.led-oficina/data',[
             'value'=>$request->value
         ]);
@@ -81,7 +81,7 @@ public function get_sala(){
 
     public function led_sala(Request $request){
         $response = Http::withHeaders([
-            'X-AIO-Key' => 'aio_PBsQ69yddxty8iPEmJ3JCY1D9GRP',
+            'X-AIO-Key' => 'aio_pwkw35gZUnGa7x9PJI2emdhisicX',
         ])->post('https://io.adafruit.com/api/v2/JuanVazquez/feeds/proyectofinal.led-sala/data',[
             'value'=>$request->value
         ]);
@@ -92,14 +92,14 @@ public function get_sala(){
 
     public function gettemperatura(Request $request){
         $response = Http::withHeaders([
-            'X-AIO-Key' => 'aio_PBsQ69yddxty8iPEmJ3JCY1D9GRP',
+            'X-AIO-Key' => 'aio_pwkw35gZUnGa7x9PJI2emdhisicX',
         ])->get('https://io.adafruit.com/api/v2/JuanVazquez/feeds/proyectofinal.temperaturavalue');
         return $response;
     }
 
     public function gethumedad(Request $request){
         $response = Http::withHeaders([
-            'X-AIO-Key' => 'aio_PBsQ69yddxty8iPEmJ3JCY1D9GRP',
+            'X-AIO-Key' => 'aio_pwkw35gZUnGa7x9PJI2emdhisicX',
         ])->get('https://io.adafruit.com/api/v2/JuanVazquez/feeds/proyectofinal.humedad');
         return $response;
     }
@@ -107,7 +107,7 @@ public function get_sala(){
     //ALARMA
     public function alarmahumo(Request $request){
         $response = Http::withHeaders([
-            'X-AIO-Key' => 'aio_PBsQ69yddxty8iPEmJ3JCY1D9GRP',
+            'X-AIO-Key' => 'aio_pwkw35gZUnGa7x9PJI2emdhisicX',
         ])->post('https://io.adafruit.com/api/v2/JuanVazquez/feeds/proyectofinal.alarmahumo/data',[
             'value'=>'0'
         ]);
@@ -118,14 +118,14 @@ public function get_sala(){
 
     public function getfocoexterno(Request $request){
         $response = Http::withHeaders([
-            'X-AIO-Key' => 'aio_PBsQ69yddxty8iPEmJ3JCY1D9GRP',
+            'X-AIO-Key' => 'aio_pwkw35gZUnGa7x9PJI2emdhisicX',
         ])->get('https://io.adafruit.com/api/v2/JuanVazquez/feeds/proyectofinal.focoexterno');
         return $response;
     }
 
     public function postfocoexterno(Request $request){
         $response = Http::withHeaders([
-            'X-AIO-Key' => 'aio_PBsQ69yddxty8iPEmJ3JCY1D9GRP',
+            'X-AIO-Key' => 'aio_pwkw35gZUnGa7x9PJI2emdhisicX',
         ])->post('https://io.adafruit.com/api/v2/JuanVazquez/feeds/proyectofinal.focoexterno/data',[
             'value'=>$request->value
         ]);
@@ -136,7 +136,7 @@ public function get_sala(){
     //COCHERA
     public function cochera(Request $request){
         $response = Http::withHeaders([
-            'X-AIO-Key' => 'aio_PBsQ69yddxty8iPEmJ3JCY1D9GRP',
+            'X-AIO-Key' => 'aio_pwkw35gZUnGa7x9PJI2emdhisicX',
         ])->post('https://io.adafruit.com/api/v2/JuanVazquez/feeds/proyectofinal.cochera/data',[
             'value'=>$request->value
         ]);
@@ -145,7 +145,7 @@ public function get_sala(){
 
     public function getcochera(){
         $response = Http::withHeaders([
-            'X-AIO-Key' => 'aio_PBsQ69yddxty8iPEmJ3JCY1D9GRP',
+            'X-AIO-Key' => 'aio_pwkw35gZUnGa7x9PJI2emdhisicX',
         ])->get('https://io.adafruit.com/api/v2/JuanVazquez/feeds/proyectofinal.cochera');
         $es = $response->username;
         return $es;
